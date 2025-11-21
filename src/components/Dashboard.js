e988fe5926ea12cbf018cf92cb04586092f8b7f0
esodi <gershommethod02@gmail.com>
2025-11-21 17:23:33 +0300

    jj6

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { getDashboardStats } from '../services/api';
-import SimpleBarChart from './SimpleBarChart';
 import { motion } from 'framer-motion';
 import { 
   MdPeople, 
